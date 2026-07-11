e865208b45dd0b6bb57473bc5c7d417e341f0b17
abbas5609 <abbas0997700000@gmail.com>
2026-07-12 00:31:26 +0300

    Create FlutterApplication.kt

---
+++ android/app/src/main/kotlin/com/example/smart_ocr_pro/FlutterApplication.kt
@@ -0,0 +1,7 @@
+package com.example.smart_ocr_pro
+
+import io.flutter.app.FlutterApplication
+import io.flutter.embedding.android.FlutterActivity
+
+class Application : FlutterApplication()
+class MainActivity : FlutterActivity()
